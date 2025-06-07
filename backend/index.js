6c9b45fa678a5204201c04aac380e72df52aadb3
Yogashree T S <yogashreesekar@gmail.com>
2025-06-07 07:09:26 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 const app = express();
-const allowedOrigins = ['https://nodejs-auth-assignment-2.onrender.com','http://localhost:3000'];
+const allowedOrigins = ['https://nodejs-auth-assignment-2.onrender.com'];
 app.use(cors({
     origin: allowedOrigins,
     credentials: true
@@ -20,13 +21,12 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api', authRoutes);
 
-
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
     console.log('MongoDB connected');
-    app.listen(5000, () => console.log('Server started on http://localhost:5000'));
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }).catch(err => {
     console.error('MongoDB connection error:', err);
 });
